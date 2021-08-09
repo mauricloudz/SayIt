ea0be9e13c0f18bdc8bbf2fba41691722da6b1ef
Mauricio Poblete <mpoblete.s3@gmail.com>
2021-08-08 23:37:07 -0400

    Hito 1 completado

---
+++ app/models/tweet.rb
@@ -0,0 +1,41 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :liking_users, :through => :likes, :source => :user
+
+  validates :content, presence: true
+
+  paginates_per 10
+
+  def like_icon(user)
+    if self.is_liked?(user)
+      'fas fa-heart text-danger'
+    else
+      'far fa-heart text-muted'
+    end
+  end
+
+  def is_liked?(user)
+    self.liking_users.include?(user)
+  end
+
+  def like(user)
+    Like.create(user: user, tweet: self)
+  end
+
+  def remove_like(user)
+    Like.where(user: user, tweet: self).destroy_all
+  end
+
+  def count_rt
+    Tweet.where(rt_ref: self.id).count
+  end
+
+  def is_retweet?
+    self.rt_ref.present?
+  end
+
+  def tweet_ref
+    Tweet.find(self.rt_ref)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+  has_many :likes
+  has_many :liked_tweets, :through => :likes, :source => :tweet
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
